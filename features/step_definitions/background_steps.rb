f13be9c5d46c60e2ab0467996beafe3bdf51079a
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-12-06 14:17:14 +0100

    cleans up features and steps

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -1,12 +1,12 @@
 Given("the following articles exist") do |table|
-    table.hashes.each do |hash|
-        create(:article, hash)
+    table.hashes.each do |article_attrs|
+        create(:article, article_attrs)
     end
 end
 
 Given("the following users exist") do |table|
-    table.hashes.each do |hash|
-        create(:user, hash)
+    table.hashes.each do |user_attrs|
+        create(:user, user_attrs)
     end
 end
 
@@ -14,10 +14,7 @@ Given("I am on the index page") do
     visit root_path
 end
 
-
-  
 Given("I am logged in as {string}") do |email|
     user = User.find_by(email: email)
-  login_as(user)
+    login_as(user)
 end
-  
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -4,7 +4,4 @@ end
 
 When("I fill in {string} with {string}") do |input, content|
     fill_in input, with: content
-end
-
-
-  
+end 
